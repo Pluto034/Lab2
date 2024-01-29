8593732ec9cb42b0b82bf6abbb40e3731719bdda
HM-GDTR <yan3260325524@outlook.com>
2024-01-29 19:23:36 +0800

    lru_k 初步完成

--- src/buffer/lru_k_replacer.cpp
+++ src/buffer/lru_k_replacer.cpp
@@ -12,19 +12,133 @@
 
 #include "buffer/lru_k_replacer.h"
 #include "common/exception.h"
+#include "fmt/format.h"
 
 namespace bustub {
 
+struct NodeInfo {
+  frame_id_t fid_;
+  bool is_inf_;
+  time_t_ k_dis_;
+  NodeInfo() = default;
+  NodeInfo(frame_id_t fid, bool isInf, time_t_ kDis) : fid_(fid), is_inf_(isInf), k_dis_(kDis) {}
+  auto operator<(const NodeInfo &other) const -> bool {
+    if (is_inf_ != other.is_inf_) {
+      return is_inf_;
+    }
+    if (is_inf_) {
+      return k_dis_ < other.k_dis_;
+    }
+    return k_dis_ > other.k_dis_;
+  }
+};
+
+struct Lockker {
+  std::mutex &mutex_;
+  explicit Lockker(std::mutex &mutex) : mutex_(mutex) {}
+  void Lock() { mutex_.lock(); }
+  virtual ~Lockker() { mutex_.unlock(); }
+};
+
+/**
+ * 构造函数
+ * @param num_frames 最多需要存储的页面数
+ * @param k
+ * */
 LRUKReplacer::LRUKReplacer(size_t num_frames, size_t k) : replacer_size_(num_frames), k_(k) {}
 
-auto LRUKReplacer::Evict(frame_id_t *frame_id) -> bool { return false; }
+auto LRUKReplacer::Evict(frame_id_t *frame_id) -> bool {
+  Lockker _(latch_);
+  _.Lock();
+  if (node_evict_.empty()) {
+    return false;
+  }
+  bool is_inf;
+  time_t_ k_dis;
+  std::vector<NodeInfo> ve;
+  for (const auto &[k, v] : node_evict_) {
+    is_inf = v.CalcKDis(current_timestamp_, k_dis);
+    ve.emplace_back(k, is_inf, k_dis);
+  }
 
-void LRUKReplacer::RecordAccess(frame_id_t frame_id, [[maybe_unused]] AccessType access_type) {}
+  std::sort(ve.begin(), ve.end());
 
-void LRUKReplacer::SetEvictable(frame_id_t frame_id, bool set_evictable) {}
+  node_evict_.erase(ve[0].fid_);
 
-void LRUKReplacer::Remove(frame_id_t frame_id) {}
+  *frame_id = ve[0].fid_;
+  return true;
+}
 
-auto LRUKReplacer::Size() -> size_t { return 0; }
+void LRUKReplacer::RecordAccess(frame_id_t frame_id, [[maybe_unused]] AccessType access_type) {
+  Lockker _(latch_);
+  _.Lock();
+  if (node_evict_.count(frame_id) != 0U) {
+    auto &node = node_evict_.at(frame_id);
+
+    node.Access(current_timestamp_);
+    current_timestamp_++;
+
+    return;
+  }
+  if (node_store_.count(frame_id) != 0U) {
+    auto &node = node_store_.at(frame_id);
+
+    node.Access(current_timestamp_);
+    current_timestamp_++;
+
+    return;
+  }
+
+  auto tmp = LRUKNode(frame_id, k_);
+
+  tmp.Access(current_timestamp_);
+  current_timestamp_++;
+  // 默认不可换出
+  node_store_.emplace(frame_id, std::move(tmp));
+}
+
+void LRUKReplacer::SetEvictable(frame_id_t frame_id, bool set_evictable) {
+  Lockker _(latch_);
+  _.Lock();
+  if (node_evict_.count(frame_id) != 0U) {
+    if (set_evictable) {
+      return;
+    }
+    auto i_tmp = node_evict_.find(frame_id);
+    node_store_.emplace(frame_id, std::move(i_tmp->second));
+    node_evict_.erase(i_tmp);
+    return;
+  }
+
+  if (node_store_.count(frame_id) != 0U) {
+    if (not set_evictable) {
+      return;
+    }
+    auto i_tmp = node_store_.find(frame_id);
+    node_evict_.emplace(frame_id, std::move(i_tmp->second));
+    node_store_.erase(i_tmp);
+    return;
+  }
+
+  throw bustub::Exception(fmt::format("No matching pages@{}.", frame_id));
+}
+
+void LRUKReplacer::Remove(frame_id_t frame_id) {
+  Lockker _(latch_);
+  _.Lock();
+  if (node_evict_.count(frame_id) != 0U) {
+    node_evict_.erase(frame_id);
+    return;
+  }
+  if (node_store_.count(frame_id) != 0U) {
+    throw bustub::Exception(fmt::format("No page@{} is not Evictable.", frame_id));
+  }
+}
+
+auto LRUKReplacer::Size() -> size_t {
+  Lockker _(latch_);
+  _.Lock();
+  return node_evict_.size();
+}
 
 }  // namespace bustub
--- src/include/buffer/lru_k_replacer.h
+++ src/include/buffer/lru_k_replacer.h
@@ -12,6 +12,8 @@
 
 #pragma once
 
+#include <algorithm>
+#include <forward_list>
 #include <limits>
 #include <list>
 #include <mutex>  // NOLINT
@@ -25,15 +27,77 @@ namespace bustub {
 
 enum class AccessType { Unknown = 0, Lookup, Scan, Index };
 
+using time_t_ = size_t;
+using history_t = std::list<time_t_>;
+
 class LRUKNode {
+  constexpr static time_t_ N_TIME = static_cast<time_t_>(-1);
+
+ public:
+  explicit LRUKNode(frame_id_t fid, size_t k) : k_(k), fid_(fid) {
+    // 一开始没有k次访问
+    size_ = 0;
+    k_last_ = history_.end();
+  }
+
+  // 添加一次访问
+  auto Access(time_t_ timestamp) -> void {
+    size_++;
+    history_.push_front(timestamp);
+    if (size_ == 1) {
+      k_last_ = history_.begin();
+    }
+    if (size_ <= k_) {
+      return;
+    }
+    k_last_--;
+    Clean();
+  };
+
+  /**
+   * @param cur_time 当前时间戳
+   * @param k_dis 返回的k_dis
+   *
+   * @return 返回是否是无穷
+   * */
+  auto CalcKDis(time_t_ cur_time, time_t_ &k_dis) const -> bool {
+    if (size_ < k_) {
+      if (size_ == 0) {
+        k_dis = N_TIME;
+      } else {
+        k_dis = *k_last_;
+      }
+      return true;
+    }
+    k_dis = cur_time - *k_last_;
+    return false;
+  }
+
  private:
+  auto Clean() -> void {
+    // 如果小于k的两倍, 不清理
+    if (size_ <= k_ * 2) {
+      return;
+    }
+    size_ = k_;
+    if (k_last_ != history_.end()) {
+      k_last_ = history_.erase(std::next(k_last_), history_.end());
+    }
+  }
+
   /** History of last seen K timestamps of this page. Least recent timestamp stored in front. */
   // Remove maybe_unused if you start using them. Feel free to change the member variables as you want.
 
-  [[maybe_unused]] std::list<size_t> history_;
-  [[maybe_unused]] size_t k_;
+  // 历史记录的长度(不是所有历史的长度, 历史会定期清理)
+  size_t size_;
+  // 第k个元素的迭代器, 如果没有k个元素就指向end
+  std::list<time_t_>::iterator k_last_;
+
+  size_t k_;
+  // 页的编号
   [[maybe_unused]] frame_id_t fid_;
-  [[maybe_unused]] bool is_evictable_{false};
+  // 访问的历史记录
+  std::list<time_t_> history_;
 };
 
 /**
@@ -71,7 +135,7 @@ class LRUKReplacer {
    * TODO(P1): Add implementation
    *
    * @brief Find the frame with largest backward k-distance and evict that frame. Only frames
-   * that are marked as 'evictable' are candidates for eviction.
+   * that are marked as 'Evictable' are candidates for eviction.
    *
    * A frame with less than k historical references is given +inf as its backward k-distance.
    * If multiple frames have inf backward k-distance, then evict frame with earliest timestamp
@@ -103,33 +167,33 @@ class LRUKReplacer {
   /**
    * TODO(P1): Add implementation
    *
-   * @brief Toggle whether a frame is evictable or non-evictable. This function also
-   * controls replacer's size. Note that size is equal to number of evictable entries.
+   * @brief Toggle whether a frame is Evictable or non-Evictable. This function also
+   * controls replacer's size. Note that size is equal to number of Evictable entries.
    *
-   * If a frame was previously evictable and is to be set to non-evictable, then size should
-   * decrement. If a frame was previously non-evictable and is to be set to evictable,
+   * If a frame was previously Evictable and is to be set to non-Evictable, then size should
+   * decrement. If a frame was previously non-Evictable and is to be set to Evictable,
    * then size should increment.
    *
    * If frame id is invalid, throw an exception or abort the process.
    *
    * For other scenarios, this function should terminate without modifying anything.
    *
-   * @param frame_id id of frame whose 'evictable' status will be modified
-   * @param set_evictable whether the given frame is evictable or not
+   * @param frame_id id of frame whose 'Evictable' status will be modified
+   * @param set_evictable whether the given frame is Evictable or not
    */
   void SetEvictable(frame_id_t frame_id, bool set_evictable);
 
   /**
    * TODO(P1): Add implementation
    *
-   * @brief Remove an evictable frame from replacer, along with its access history.
+   * @brief Remove an Evictable frame from replacer, along with its access history.
    * This function should also decrement replacer's size if removal is successful.
    *
    * Note that this is different from evicting a frame, which always remove the frame
    * with largest backward k-distance. This function removes specified frame id,
    * no matter what its backward k-distance is.
    *
-   * If Remove is called on a non-evictable frame, throw an exception or abort the
+   * If Remove is called on a non-Evictable frame, throw an exception or abort the
    * process.
    *
    * If specified frame is not found, directly return from this function.
@@ -141,7 +205,7 @@ class LRUKReplacer {
   /**
    * TODO(P1): Add implementation
    *
-   * @brief Return replacer's size, which tracks the number of evictable frames.
+   * @brief Return replacer's size, which tracks the number of Evictable frames.
    *
    * @return size_t
    */
@@ -150,7 +214,7 @@ class LRUKReplacer {
  private:
   // TODO(student): implement me! You can replace these member variables as you like.
   // Remove maybe_unused if you start using them.
-  [[maybe_unused]] std::unordered_map<frame_id_t, LRUKNode> node_store_;
+  [[maybe_unused]] std::unordered_map<frame_id_t, LRUKNode> node_evict_, node_store_;
   [[maybe_unused]] size_t current_timestamp_{0};
   [[maybe_unused]] size_t curr_size_{0};
   [[maybe_unused]] size_t replacer_size_;
