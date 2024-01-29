bea210a10514d4d8a24d93d7218e66368c9bf8f6
HM-GDTR <yan3260325524@outlook.com>
2024-01-29 20:53:59 +0800

    Task#2 磁盘操作队列

--- src/storage/disk/disk_scheduler.cpp
+++ src/storage/disk/disk_scheduler.cpp
@@ -18,9 +18,9 @@ namespace bustub {
 
 DiskScheduler::DiskScheduler(DiskManager *disk_manager) : disk_manager_(disk_manager) {
   // TODO(P1): remove this line after you have implemented the disk scheduler API
-  throw NotImplementedException(
-      "DiskScheduler is not implemented yet. If you have finished implementing the disk scheduler, please remove the "
-      "throw exception line in `disk_scheduler.cpp`.");
+  /*  throw NotImplementedException(
+        "DiskScheduler is not implemented yet. If you have finished implementing the disk scheduler, please remove the "
+        "throw exception line in `disk_scheduler.cpp`.");*/
 
   // Spawn the background thread
   background_thread_.emplace([&] { StartWorkerThread(); });
@@ -34,8 +34,29 @@ DiskScheduler::~DiskScheduler() {
   }
 }
 
-void DiskScheduler::Schedule(DiskRequest r) {}
+void DiskScheduler::Schedule(DiskRequest r) {
+  if (r.is_write_) {
+    disk_manager_->WritePage(r.page_id_, r.data_);
+  } else {
+    disk_manager_->ReadPage(r.page_id_, r.data_);
+  }
 
-void DiskScheduler::StartWorkerThread() {}
+  r.callback_.set_value(true);
+}
+
+void DiskScheduler::StartWorkerThread() {
+  while (true) {
+    auto req = request_queue_.Get();
+    // 如果还有待执行的
+    while (req.has_value()) {
+      Schedule(std::move(req.value()));
+
+      req = request_queue_.Get();
+    }
+    if (req == std::nullopt) {
+      return;
+    }
+  }
+}
 
 }  // namespace bustub
