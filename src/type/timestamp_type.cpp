696863b236e1fb267e53d9b61c3ee2d7e47775eb
RainbowX <yan3260325524@outlook.com>
2024-09-10 09:01:34 +0800

    fix: zone buffer too short.

--- src/type/timestamp_type.cpp
+++ src/type/timestamp_type.cpp
@@ -115,7 +115,7 @@ auto TimestampType::ToString(const Value &val) const -> std::string {
   tm /= 32;
   auto month = static_cast<uint16_t>(tm);
   const size_t date_str_len = 30;
-  const size_t zone_len = 5;
+  const size_t zone_len = 20;
   char str[date_str_len];
   char zone[zone_len];
   snprintf(str, date_str_len, "%04d-%02d-%02d %02d:%02d:%02d.%06d", year, month, day, hour, min, sec, micro);
